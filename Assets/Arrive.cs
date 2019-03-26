3f9f0a78f6dd3889866b5c079dcada2cdd681f38
CFerry <c16349346@mydit.ie>
2019-03-26 11:02:45 +0000

    Tiberium and Spawning

--- Assets/Arrive.cs
+++ Assets/Arrive.cs
@@ -9,6 +9,34 @@ public class Arrive: SteeringBehaviour
     public Vector3 targetPosition = Vector3.zero;
     public float slowingDistance = 15.0f;
 
+    public GameObject[] TargetBases;
+    
+        
+    public override Vector3 Calculate()
+    {
+        return boid.ArriveForce(targetPosition, slowingDistance);
+    }
+
+    public void Update()
+    {
+        int rnd = UnityEngine.Random.Range(0, 3);
+
+        TargetBases = new GameObject[4];
+
+        targetPosition = TargetBases[rnd].transform.position;
+        /*if (targetGameObject != null)
+        {
+            targetPosition = targetGameObject.transform.position;
+        }*/
+    }
+}
+
+
+
+/*
+ *  public Vector3 targetPosition = Vector3.zero;
+    public float slowingDistance = 15.0f;
+
     public GameObject targetGameObject = null;
         
     public override Vector3 Calculate()
@@ -23,4 +51,4 @@ public class Arrive: SteeringBehaviour
             targetPosition = targetGameObject.transform.position;
         }
     }
-}
+*/
--- Assets/Base.cs
+++ Assets/Base.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 
 public class Base : MonoBehaviour
 {
-    public float tiberium = 0;
+    public float tiberium = 5;
 
     public TextMeshPro text;
 
     public GameObject fighterPrefab;
+
+    private IEnumerator coroutine;
 
 
     // Start is called before the first frame update
@@ -19,11 +21,49 @@ public class Base : MonoBehaviour
         {
             r.material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
         }
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         text.text = "" + tiberium;
+
+        Resources();
+
+    }
+
+    void Resources()
+    {
+        if(tiberium <= 10)
+        {
+            coroutine = AccumulateTiberium();
+            StartCoroutine(coroutine);
+            //StopCoroutine(coroutine);
+           
+        }
+         else if(tiberium >= 10)
+        {
+            StopCoroutine(coroutine);
+            Spawn();  
+        }
+        
+    }
+
+
+    private void Spawn()
+    {
+        Instantiate(fighterPrefab, this.transform.position, Quaternion.identity);
+        tiberium -= 10;
+    }
+
+ 
+    IEnumerator AccumulateTiberium()
+    {
+        yield return new WaitForSeconds(1.0f);
+        tiberium = tiberium + (1 * Time.deltaTime);
+        StartCoroutine(coroutine);
+
     }
 }
